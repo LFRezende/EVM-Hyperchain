5b4b08197015421da881a7070e4ab03090168425
LFRezende <lfsrsprofessional@gmail.com>
2023-06-01 18:15:19 -0300

    Hardhat node operational

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 let connectEl = document.getElementById("connectButton");
+import { abi, contractAddress } from "./constants.js";
+import { ethers } from "./ethers-5.1.esm.min.js";
+connectEl.onclick = addStudent;
 async function connect() {
   if (typeof window.ethereum != "undefined") {
     console.log("There is an Ethereum Based Wallet Available.");
@@ -7,9 +10,9 @@ async function connect() {
   }
 }
 
-function addStudent(){
-  if (typeof window.ethereum != "undefined"){
+function addStudent() {
+  if (typeof window.ethereum != "undefined") {
     console.log("EVM-Wallet ok.");
-    const 
+    const cttAddress = contractAddress;
   }
 }
